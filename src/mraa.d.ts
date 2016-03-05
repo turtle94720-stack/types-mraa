40ed3329286d6f3069de0f40275c96c1b1c64794
turtle94720-stack <turtle94720@gmail.com>
2016-03-04 18:30:44 -0800

    Add typescript definition files

---
+++ src/mraa.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for MRAA v0.9.5
+// Project: https://github.com/intel-iot-devkit/mraa
+// Definitions by: Snow Crab Software, LLC <https://github.com/snowcrab/>
+// Definitions: https://github.com/snowcrab/mraa-typescript
