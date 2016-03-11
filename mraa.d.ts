a21c77bf140920f49d3d785eb938cc5b45d92e22
turtle94720-stack <turtle94720@gmail.com>
2016-03-11 15:04:02 -0800

    Add Gpio definition

--- mraa.d.ts
+++ mraa.d.ts
@@ -1,4 +1,7 @@
-// Type definitions for MRAA v0.9.5
+// Type definitions for MRAA v0.9.6
 // Project: https://github.com/intel-iot-devkit/mraa
 // Definitions by: Snow Crab Software, LLC <https://github.com/snowcrab/>
 // Definitions: https://github.com/snowcrab/mraa-typescript
+
+export * from './src/common'
+export * from './src/gpio';
